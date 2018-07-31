93e4bceafb6554f1d66fe7aa3955f9f0a9918411
sayaMahi <tech.mahipal.reddy@gmail.com>
2018-07-31 16:08:05 +0530

    added Contract, dbHelper, Content Provider

---
+++ app/src/main/java/www/androidcitizen/com/popularmoviesone/data/database/FavContract.java
@@ -0,0 +1,62 @@
+package www.androidcitizen.com.popularmoviesone.data.database;
+
+import android.net.Uri;
+import android.provider.BaseColumns;
+
+/**
+ * Created by Mahi on 31/07/18.
+ * www.androidcitizen.com
+ */
+
+public class FavContract {
+
+    /*
+    *  Step 1: Implement Contract Class:                    [ ****  THIS Class **** ]
+    *
+    *           -> Base Columns for Required Table.
+    *           -> All Columns to be mentioned.
+    *
+    *  Step 2: Implement SQLiteOpenHelper Class:
+    *           -> Database Name and its Version.
+    *           -> Create Constructor, onCreate (create Table, execute), onUpgrade (delete, oncreate)
+    *
+    *  Step 3: Implement Content URI in Contract Class      [ ****  THIS Class **** ]
+    *
+    *           -> Set Authority, URI, path to identify the Table
+    *           -> build URI.
+    *
+    *  Step 4: Implement Content Provider Class.
+    *
+    *           -> Implement Uri Matcher using Content URIs defined.
+    *           -> Implement CRUD Methods with Data Validation.
+    *
+     *  Step 5: Use Content Resolver (from activity) to access CRUD methods from Content Provider.
+    *
+    *  Mention Provider in Manifest file.
+    */
+
+    public static final String AUTHORITY = "www.androidcitizen.com.popularmoviesone";
+    public static final String PATH_FAV_MOVIES = "favorites";
+
+    private static final Uri BASE_CONTENT_URI = Uri.parse("content://" + AUTHORITY);
+
+    private FavContract() {}
+
+    public static final class FavMovieEntry implements BaseColumns {
+
+        public static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon().appendPath(PATH_FAV_MOVIES).build();
+
+        public static final String TABLE_NAME               = "fav_movies";
+
+        public static final String _ID                      = BaseColumns._ID;
+        public static final String COLUMN_MOVIE_ID          = "movie_id";
+        public static final String COLUMN_ORIGINAL_TITLE    = "original_title";
+        public static final String COLUMN_POSTER_PATH       = "poster_path";
+        public static final String COLUMN_BACKDROP_PATH     = "backdrop_path";
+        public static final String COLUMN_RELEASE_DATE      = "release_date";
+        public static final String COLUMN_OVERVIEW          = "overview";
+        public static final String COLUMN_VOTE_AVERAGE      = "vote_average";
+
+    }
+
+}
