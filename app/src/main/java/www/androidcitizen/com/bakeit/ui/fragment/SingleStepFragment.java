9e6c88c67e5c39fbf9e569a34d5603652e1520c7
sayaMahi <tech.mahipal.reddy@gmail.com>
2018-09-03 21:52:16 +0530

    ui fixes

--- app/src/main/java/www/androidcitizen/com/bakeit/ui/fragment/SingleStepFragment.java
+++ app/src/main/java/www/androidcitizen/com/bakeit/ui/fragment/SingleStepFragment.java
@@ -147,22 +147,10 @@ public class SingleStepFragment extends Fragment {
     @Override
     public void onResume() {
         super.onResume();
-        hideSystemUi();
+
         if ((Util.SDK_INT <= 23 || player == null)) {
             initializePlayer();
         }
-    }
-
-    @SuppressLint("InlinedApi")
-    private void hideSystemUi() {
-
-        singleStepBinding.playerView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
-                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
     }
 
     @Override
